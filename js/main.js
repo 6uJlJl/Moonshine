cb45a17c3ad35d062d59756ae4945a8cfc7a6812
Alexey Merkulov <bgates@inbox.ru>
2019-08-31 10:28:12 +0300

    Сверстан блок offers с адаптивом

--- js/main.js
+++ js/main.js
@@ -29,6 +29,8 @@ $("body").click (function(event){
 
 })
 
+// СЛАЙДЕРЫ
+
 $('.dymka-15l-small-slider').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
@@ -44,4 +46,86 @@ $('.dymka-15l-big-slider').slick({
   fade: true,
   asNavFor: '.dymka-15l-small-slider'
 });
+
+$('.germany-14l-small-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.germany-14l-big-slider',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+$('.germany-14l-big-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.germany-14l-small-slider'
+});
+
+$('.wein4-12l-small-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.wein4-12l-big-slider',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+$('.wein4-12l-big-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.wein4-12l-small-slider'
+});
+
+$('.wein4pro-20l-small-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.wein4pro-20l-big-slider',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+$('.wein4pro-20l-big-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.wein4pro-20l-small-slider'
+});
+
+$('.wein5-20l-small-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.wein5-20l-big-slider',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+$('.wein5-20l-big-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.wein5-20l-small-slider'
+});
+
+$('.wein5pro-37l-small-slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.wein5pro-37l-big-slider',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+$('.wein5pro-37l-big-slider').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.wein5pro-37l-small-slider'
+});
+
+
 
